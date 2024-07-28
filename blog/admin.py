fadbbab54bc2e5bda928548f37781e0dd465b6f5
hamidreza <keramathamidreza138361@gmail.com>
2024-07-28 11:09:03 +0330

    add login and signup

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,6 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'datetime_modified')
-    ordering = ('status',)
+
 
 # admin.site.register(Post, PostAdmin)
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class BlogPostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # def setUp(self):
+        user = User.objects.create(username='user1')
+        cls.Post1 = Post.objects.create(
+            title='Post1',
+            text='This is the description of Post1',
+            status=Post.STATUS_CHOICES[0][0],
+            author=user,
+        )
+        cls.post2 = Post.objects.create(
+            title='Post2',
+            text='Lorem Ipsum post2',
+            status=Post.STATUS_CHOICES[1][0],
+            author=user,
+        )
+
+    def test_post_list_url_by_name(self):
+        response = self.client.get(reverse('posts_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_url(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_title_on_blog_list_page(self):
+        response = self.client.get(reverse('posts_list'))
+        self.assertContains(response, self.post1.title)
+
+    def test_post_details_on_blog_detail_page(self):
+        response = self.client.get(f'/blog/{self.post1.id}/')
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post1.text)
